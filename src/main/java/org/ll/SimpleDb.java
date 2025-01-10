36dc45b24a7fd1b9bb2811952ba235d39deb031c
leemimi <mijeong1015@naver.com>
2025-01-10 23:35:50 +0900

    first commit

---
+++ src/main/java/org/ll/SimpleDb.java
@@ -0,0 +1,9 @@
+package org.ll;
+
+public class SimpleDb {
+    public SimpleDb (String localhost, String root, String lldj123414, String simpleDb__test) {
+    }
+
+    public void setDevMode (boolean b) {
+    }
+}
